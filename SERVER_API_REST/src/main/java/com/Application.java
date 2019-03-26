7cbed1b4a8ec39f3713a3b229592af25a67af4fa
boulestinn <nathan.boulestin@reseau.eseo.fr>
2019-03-26 14:06:05 +0100

    1er commit

---
+++ SERVER_API_REST/src/main/java/com/Application.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application 
+{
+    public static void main( String[] args )
+    {
+    	try {
+    	SpringApplication.run(Application.class, args);
+    	System.out.println("Application démarée !");
+    	}catch (Exception e){
+    		System.err.println(e.getStackTrace());
+    	}
+    }
+}
---
+++ SERVER_API_REST/src/main/java/com/dao/DaoVille.java
@@ -0,0 +1,32 @@
+package com.dao;
+
+import java.sql.Connection;
+
+public class DaoVille extends Dao {
+
+	public DaoVille(Connection conn) {
+		super(conn);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean create(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean delete(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean update(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
