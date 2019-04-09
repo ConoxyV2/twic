d92fdd16ee70d00447077b73db51dcdbb7c1359c
boulestinn <nathan.boulestin@reseau.eseo.fr>
2019-04-09 14:36:07 +0200

    afternoom commit

--- SERVER_API_REST/src/main/java/com/controller/VilleController.java
+++ SERVER_API_REST/src/main/java/com/controller/VilleController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,42 +22,27 @@ public class VilleController {
 
 	private DaoVille daoVille = new com.dao.DaoVille();
 	
+	@RequestMapping(value="/ville", method=RequestMethod.GET)
+	@ResponseBody
+	public List<Ville> get(@RequestParam(value="codePostal") String codePostal) {
+		return daoVille.find(codePostal);
+	}
+	
 	@RequestMapping(value="/ville", method=RequestMethod.POST)
 	@ResponseBody
-	public List<Ville> post(@RequestParam Map<String,String> allParams) {
-		List<Ville> villes = new ArrayList<Ville>();
-		Set<Entry<String, String>> set = allParams.entrySet();
-		for(Entry<String, String> entry : set) {
-			villes.addAll(daoVille.find(entry.getValue()));
-		}
-		return villes;
+	public boolean post(@RequestBody Ville ville) {
+		return daoVille.create(ville);
 	}
 	
 	@RequestMapping(value="/ville", method=RequestMethod.PUT)
 	@ResponseBody
-	public boolean put(@RequestParam String ville) {
-		Ville v = new Ville();
-		v.setCode_commune_INSEE("a");
-		v.setCode_postal("b");
-		v.setLatitude("c");
-		v.setLibelle_acheminement("d");
-		v.setLigne_5("e");
-		v.setLongitude("f");
-		v.setNom_commune(ville);
-		return daoVille.create(v);
+	public boolean put(@RequestBody List<Ville> villes) {
+		return daoVille.update(villes.get(0), villes.get(1));
 	}
 	
 	@RequestMapping(value="/ville", method=RequestMethod.DELETE)
 	@ResponseBody
-	public boolean delete(@RequestParam String ville) {
-		Ville v = new Ville();
-		v.setCode_commune_INSEE("a");
-		v.setCode_postal("b");
-		v.setLatitude("c");
-		v.setLibelle_acheminement("d");
-		v.setLigne_5("e");
-		v.setLongitude("f");
-		v.setNom_commune(ville);
-		return daoVille.delete(v);
+	public boolean delete(@RequestBody Ville ville) {
+		return daoVille.delete(ville);
 	}
 }
--- SERVER_API_REST/src/main/java/com/dao/DaoVille.java
+++ SERVER_API_REST/src/main/java/com/dao/DaoVille.java
@@ -1,6 +1,7 @@
 package com.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,14 +10,22 @@ import java.util.List;
 
 import com.dto.Ville;
 
-public class DaoVille extends Dao {
+public class DaoVille {
 	
+	protected Connection connect = null;
 	private final String SQL_CREATE_VILLE = "INSERT INTO ville_france (Code_commune_INSEE,Nom_commune,Code_postal,Libelle_acheminement,Ligne_5,Latitude,Longitude) VALUES (?,?,?,?,?,?,?);";
+	private final String SQL_UPDATE_VILLE = "UPDATE ville_france SET Code_commune_INSEE=?,Nom_commune=?,Code_postal=?,Libelle_acheminement=?,Ligne_5=?,Latitude=?,Longitude=? WHERE Code_commune_INSEE=?,Nom_commune=?,Code_postal=?,Libelle_acheminement=?,Ligne_5=?,Latitude=?,Longitude=?;";
 	private final String SQL_DELETE_VILLE = "DELETE FROM ville_france WHERE Code_commune_INSEE=? AND Nom_commune=? AND Code_postal=? AND Libelle_acheminement=? AND Ligne_5=? AND Latitude=? AND Longitude=?;";
-	private final String SQL_SELECT_VILLE = "SELECT * FROM ville_france WHERE Code_commune_INSEE LIKE(?) OR Nom_commune LIKE(?) OR Code_postal LIKE(?) OR Libelle_acheminement LIKE(?) OR Ligne_5 LIKE(?) OR Latitude LIKE(?) OR Longitude LIKE(?);";
+	private final String SQL_SELECT_VILLE_CP = "SELECT * FROM ville_france WHERE Code_postal=?;";
 	
 	public DaoVille() {
-		super();
+		try {
+			DriverManager.deregisterDriver(new com.mysql.jdbc.Driver());
+			connect = DriverManager.getConnection("jdbc:mysql://localhost:3308/maven_tp1?user=admin&password=admin");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public boolean create(Object obj) {
@@ -53,9 +62,32 @@ public class DaoVille extends Dao {
 		}
 	}
 
-	public boolean update(Object obj) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(Ville villeA, Ville villeN) {
+		try {
+			PreparedStatement preparedStm = this.connect.prepareStatement(SQL_DELETE_VILLE);
+			preparedStm.setString(1, villeN.getCode_commune_INSEE());
+			preparedStm.setString(2, villeN.getNom_commune());
+			preparedStm.setString(3, villeN.getCode_postal());
+			preparedStm.setString(4, villeN.getLibelle_acheminement());
+			preparedStm.setString(5, villeN.getLigne_5());
+			preparedStm.setString(6, villeN.getLatitude());
+			preparedStm.setString(7, villeN.getLongitude());
+			preparedStm.setString(8, villeA.getCode_commune_INSEE());
+			preparedStm.setString(9, villeA.getNom_commune());
+			preparedStm.setString(10, villeA.getCode_postal());
+			preparedStm.setString(11, villeA.getLibelle_acheminement());
+			preparedStm.setString(12, villeA.getLigne_5());
+			preparedStm.setString(13, villeA.getLatitude());
+			preparedStm.setString(14, villeA.getLongitude());
+			if(preparedStm.executeUpdate()==1) {
+				return true;
+			}else {
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	public Object find(int id) {
@@ -63,20 +95,13 @@ public class DaoVille extends Dao {
 		return null;
 	}
 	
-	public List<Ville> find(String str) {
-		str = "%"+str+"%";
+	public List<Ville> find(String cp) {
 		List<Ville> villes = new ArrayList<Ville>();
 		PreparedStatement preparedStm = null;
 		ResultSet rSet = null;
 		try {
-			preparedStm = this.connect.prepareStatement(SQL_SELECT_VILLE);
-			preparedStm.setString(1, str);
-			preparedStm.setString(2, str);
-			preparedStm.setString(3, str);
-			preparedStm.setString(4, str);
-			preparedStm.setString(5, str);
-			preparedStm.setString(6, str);
-			preparedStm.setString(7, str);
+			preparedStm = this.connect.prepareStatement(SQL_SELECT_VILLE_CP);
+			preparedStm.setString(1, cp);
 			rSet = preparedStm.executeQuery();
 			while(rSet.next()) {
 				Ville ville = new Ville();
